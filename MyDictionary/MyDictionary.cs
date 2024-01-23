9aa7987c836c97d1ba3eddc84dcd2c08aa5d3a43
umutgulfidan <umutgulfidan@gmail.com>
2024-01-23 11:46:18 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<TKey,TValue>
+    {
+        List<TKey> keys;
+        List<TValue> values;
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return Get(key);
+            }
+            set
+            {
+                values[keys.IndexOf(key)] = value;
+            }
+        }
+        public MyDictionary()
+        {
+            keys = new List<TKey>();
+            values = new List<TValue>();
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (keys.Contains(key))
+            {
+                return values[keys.IndexOf(key)];
+            }
+            throw new Exception("Anahtar Bulunumadı");
+        }
+        public void Add(TKey key, TValue value)
+        {
+            keys.Add(key);
+            values.Add(value);
+        }
+        public void Remove(TKey key)
+        {
+            int index = keys.IndexOf(key);
+            if(index != -1)
+            {
+                keys.RemoveAt(index);
+                values.RemoveAt(index);
+            }
+        }
+        public void Clear()
+        {
+            keys.Clear();
+            values.Clear();
+        }
+        public bool ContainsKey(TKey key)
+        {
+            foreach (var item in keys)
+            {
+                if (item.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void PrintAll()
+        {
+            foreach (var item in keys)
+            {
+                Console.WriteLine("Key: "+item+"  /   Value :"+Get(item));
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int> myDictionary = new MyDictionary<string,int>();
+            myDictionary.Add("a", 1);
+            myDictionary.Add("b", 2);
+            myDictionary.Add("c", 3);
+            myDictionary.PrintAll();
+            CizgiCek();
+            myDictionary.Add("d", 4);
+            myDictionary.Remove("a");
+            myDictionary.PrintAll();
+            CizgiCek();
+            myDictionary["b"] = -1;
+            myDictionary["c"] = 0;
+            myDictionary.PrintAll();
+            CizgiCek();
+            myDictionary.Clear();
+            myDictionary.PrintAll();
+            CizgiCek();
+            Console.ReadLine();
+            
+        }
+        public static void CizgiCek()
+        {
+            Console.WriteLine("-------------------------------------");
+        }
+    }
+}
